fc09367169246e4686bc73e8b6084a4b5217089a
nshupinski <55292866+nshupinski@users.noreply.github.com>
2020-12-05 21:40:44 -0500

    attempted to create like button on pet cards

--- 3342_TermProject-PetAdoption/3342_TermProject-PetAdoption/Javascript/Add_Pets.js
+++ 3342_TermProject-PetAdoption/3342_TermProject-PetAdoption/Javascript/Add_Pets.js
@@ -41,7 +41,35 @@ function createPetCard(name, userID, animal, breed, gwKids, gwPets, location, ag
     ageRange_display.setAttribute("style", "margin-bottom: 0;");
     ageRange_display.innerHTML = "Age-Range: " + age;
 
+    var likeSec = document.createElement("div");
+    likeSec.setAttribute("style", "border-style: solid; width: 16em; height: 3em; background-color: white;");
+
+    var level = document.createElement("a");
+    level.setAttribute("class", "level-item");
+    level.setAttribute("aria-label", "like");
+
+    var icon = document.createElement("span");
+    icon.setAttribute("class", "icon-is-small");
+
+    var fas = document.createElement("i");
+    fas.setAttribute("class", "fas fa-heart");
+    fas.setAttribute("aria-hidden", "true");
+
+    /*var heart = document.createElement("svg");
+    heart.setAttribute("width", "1em");
+    heart.setAttribute("height", "1em");
+    heart.setAttribute("viewBox", "0 0 16 16");
+    heart.setAttribute("class", "bi bi-heart-fill");
+    heart.setAttribute("fill", "white");
+    heart.setAttribute("xmlns", "http://www.w3.org/2000/svg"); 
+
+    var path = document.createElement("path");
+    path.setAttribute("fill-rule", "evenodd");
+    path.setAttribute("d", "M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z");*/
+
+
     card.appendChild(image);
+
     body.appendChild(name_display);
     body.appendChild(animal_display);
     body.appendChild(breed_display);
@@ -49,7 +77,16 @@ function createPetCard(name, userID, animal, breed, gwKids, gwPets, location, ag
     body.appendChild(gwPets_display);
     body.appendChild(location_display);
     body.appendChild(ageRange_display);
+
+    icon.appendChild(fas);
+    level.appendChild(icon);
+    likeSec.appendChild(level);
+    //heart.appendChild(path);
+    //likeSec.appendChild(heart);
+
+    body.appendChild(likeSec);
     card.appendChild(body);
+
     var col = document.getElementById("col" + colNum);
     col.appendChild(card);
 
